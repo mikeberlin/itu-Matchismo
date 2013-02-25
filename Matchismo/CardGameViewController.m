87bc220654ca2312baf8baca516011fff3eef722
Mike Berlin <mikeberlin@Home-Base.local>
2013-02-25 00:32:02 -0600

    Learning to use source control in Xcode.  Should be last check in for Assignment 0.

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -7,12 +7,43 @@
 //
 
 #import "CardGameViewController.h"
+#import "PlayingCardDeck.h"
+#import "PlayingCard.h"
 
 @interface CardGameViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipCount;
+@property (strong, nonatomic) PlayingCardDeck *myDeck;
 @end
 
 @implementation CardGameViewController
+
+- (PlayingCardDeck *)myDeck
+{
+    if (_myDeck == nil) _myDeck = [[PlayingCardDeck alloc] init];
+    return _myDeck;
+}
+
+- (void)setFlipCount:(int)flipCount
+{
+    _flipCount = flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
+}
+
+- (IBAction)flipCard:(UIButton *)sender
+{
+    sender.selected = !sender.isSelected;
+
+    if (sender.selected)
+    {
+        PlayingCard *nextCard = [[PlayingCard alloc] init];
+        nextCard = (PlayingCard *)[self.myDeck drawRandomCard];
+
+        [sender setTitle:nextCard.contents forState:UIControlStateSelected];
+    }
+
+    self.flipCount++;
+}
 
 - (void)viewDidLoad
 {
